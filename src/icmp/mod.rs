f0a83eab350ed209b3038efeca9689025b590d18
Felipe Espinoza <yayofelipe@gmail.com>
2018-07-06 12:29:17 -0400

    Parralelise icmp packet read

--- src/icmp/mod.rs
+++ src/icmp/mod.rs
@@ -22,11 +22,17 @@ impl IcmpHandler {
     }
 
     pub fn run(&mut self) {
-        let target : Ipv4Addr = "172.30.65.176".parse().unwrap();
-        for _ in 0..1000000 {
-            self.writer.send_icmp(target);
+        let read = self.reader.run();
+        let target : Ipv4Addr = "1.1.1.1".parse().unwrap();
+        for _ in 0..10 {
+            //self.writer.send_icmp(target);
         }
+        use std::{thread, time};
+        thread::sleep(time::Duration::from_millis(10000));
+        while let Ok(_) = read.try_recv() {
+            println!("x");
+        }
+
         println!("ended");
-        //self.reader.run();
     }
 }
--- src/icmp/reader.rs
+++ src/icmp/reader.rs
@@ -2,8 +2,10 @@ extern crate pnet;
 
 use pnet::packet::Packet;
 use pnet::packet::icmp::{IcmpPacket, IcmpTypes};
+use pnet::packet::icmp::echo_reply::{EchoReply, EchoReplyPacket};
 use pnet::packet::ip::IpNextHeaderProtocols;
-use pnet::packet::ipv4::{Ipv4Packet};
+use pnet::packet::ipv4::Ipv4Packet;
+use pnet::packet::FromPacket;
 use pnet::transport::{transport_channel, TransportReceiver, ipv4_packet_iter};
 
 use pnet::transport::TransportChannelType::Layer3;
@@ -14,9 +16,19 @@ use std::cell::RefCell;
 
 use icmp::writer::IcmpWriter;
 
+use std::thread;
+use std::sync::mpsc;
+use std::sync::Mutex;
+use std::sync::Arc;
+
 pub struct IcmpReader {
-    reader: RefCell<TransportReceiver>,
+    reader: Arc<Mutex<RefCell<TransportReceiver>>>,
     _local: Ipv4Addr,
+}
+
+pub struct IcmpResponce {
+    source: Ipv4Addr,
+    icmp: EchoReply,
 }
 
 impl IcmpReader {
@@ -32,40 +44,59 @@ impl IcmpReader {
         };
 
         return (IcmpReader {
-            reader: RefCell::new(rx),
+            reader: Arc::new(Mutex::new(RefCell::new(rx))),
             _local: local,
         }, IcmpWriter::new(tx, local));
     }
 
-    pub fn run(&mut self) {
-        let mut reader = self.reader.try_borrow_mut().unwrap();
-        let mut reader = ipv4_packet_iter(&mut reader);
-        loop {
-            let packet = reader.next();
-            match packet {
-                Ok((packet, _)) => {
-                    self.process_ipv4(&packet);
+    pub fn run(&mut self) -> mpsc::Receiver<IcmpResponce> {
+        let reader = self.reader.clone();
+        let local = self._local;
+
+        let (sender, receiver) = mpsc::channel::<IcmpResponce>();
+        thread::spawn(move || {
+            match reader.try_lock() {
+                Err(_) => panic!("You are not allowd to call IcmpReader::run() Twice"),
+                Ok(lock) => {
+                    let mut borrowed = lock.try_borrow_mut().unwrap();
+                    let mut iter = ipv4_packet_iter(&mut borrowed);
+                    loop {
+                        let packet = iter.next();
+                        if let Ok((packet, _)) = packet {
+                            if let Err(_) = Self::process_ipv4(&packet, local, &sender) {
+                                // Channel is closed, exit
+                                return;
+                            }
+                        }
+                    }
                 }
-                Err(_) => {}
-            }
-        }
+            };          
+        });
+
+        return receiver;
     }
 
-    fn process_ipv4(&self, packet: &Ipv4Packet) {
-        match packet.get_next_level_protocol() {
-            IpNextHeaderProtocols::Icmp => self.process_icmp4(packet.payload(), &packet),
-            _ => {}
+    fn process_ipv4(packet: &Ipv4Packet, local: Ipv4Addr, sender: &mpsc::Sender<IcmpResponce>) -> Result<(), ()> {
+        if packet.get_next_level_protocol() == IpNextHeaderProtocols::Icmp {
+             return Self::process_icmp4(packet.payload(), &packet, local, sender)
         }
+        return Ok(());
     }
 
-    fn process_icmp4(&self, packet: &[u8], header: &Ipv4Packet) {
+    fn process_icmp4(packet: &[u8], header: &Ipv4Packet, local: Ipv4Addr, sender: &mpsc::Sender<IcmpResponce>) -> Result<(), ()> {
         let icmp_packet = IcmpPacket::new(packet);
         if let Some(icmp) = icmp_packet {
             match icmp.get_icmp_type() {
                 IcmpTypes::EchoReply => {
-                    println!("Reply");
-                    println!("{:?}", header.packet());
-                    //self.writer.try_borrow_mut().unwrap().send_icmp();
+                    if let Some(icmp) = EchoReplyPacket::new(&packet) {
+                        let responce = IcmpResponce {
+                            source: Ipv4Addr::from(header.get_source()),
+                            icmp: icmp.from_packet(),
+                        };
+                        if let Err(e) = sender.send(responce) {
+                            return Err(());
+                        }
+                    }
                 }
                 IcmpTypes::TimeExceeded => {
                     let src = Ipv4Addr::from(header.get_source());
@@ -78,8 +109,9 @@ impl IcmpReader {
                     let _src = Ipv4Addr::from(header.get_source());
                     //self.writer.try_borrow_mut().unwrap().send_icmp();
                 }
-                _ => {}
+                _ => {},           
             }
         }
+        return Ok(());
     }
 }
