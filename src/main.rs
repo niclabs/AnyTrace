5477ba83f698c76e1b19f4c1fe68df0499b7e804
Felipe <fdns@niclabs.cl>
2018-07-09 15:50:34 -0400

    Removing unused code

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,6 @@ use ping::{PingHandler, PingMethod};
 use std::net::Ipv4Addr;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
-/*
-fn send_udp() -> std::io::Result<()> {
-    use std::net::Ipv4Addr;
-    use std::net::SocketAddrV4;
-    use std::net::UdpSocket;
-    let socket = UdpSocket::bind("0.0.0.0:34254")?;
-
-    // Redeclare `buf` as slice of the received data and send reverse data back to origin.
-    let buf = [0u8; 0];
-    let ip: Ipv4Addr = "1.1.1.33".parse().expect("couldn't parse ip address");
-    let address = SocketAddrV4::new(ip, 34254);
-    socket.set_ttl(2).unwrap();
-    socket.send_to(&buf, address).expect("couldn't send data");
-    return Ok(());
-}
-*/
-
 fn main() {
     let handler = PingHandler::new("172.30.65.31", PingMethod::ICMP);
     let target: Ipv4Addr = "1.1.1.1".parse().unwrap();
