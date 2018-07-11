fccf70a7c70bee04801ea2b3ab02bc9777623334
Felipe <fdns@niclabs.cl>
2018-07-11 12:51:57 -0400

    Moving logic from the bin folder to a lib.

--- src/bin/hitlist.rs
+++ src/bin/hitlist.rs
@@ -1,49 +1,7 @@
-extern crate pnet;
-extern crate ping;
+extern crate anytrace;
 
-use ping::{PingHandler, PingMethod};
-use std::net::Ipv4Addr;
-use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use anytrace::hitlist::run;
 
 fn main() {
-    let localip = "10.0.2.15";
-    let handler = PingHandler::new(localip, PingMethod::ICMP);
-
-    let target: Ipv4Addr = "1.1.1.1".parse().unwrap();
-    for _ in 0..10 {
-        handler.writer.send(target);
-    }
-
-    while let Ok(packet) = handler
-        .reader
-        .reader()
-        .recv_timeout(Duration::from_millis(2000))
-    {
-        print!("{:?}, {:?}: ", packet.source, packet.ttl);
-        match packet.icmp {
-            ping::Responce::Echo(packet) => {
-                if let Ok(ts) = PingHandler::get_packet_timestamp_ms(&packet.payload, true) {
-                    println!("Parsed correctly, delta(ms): {}", time_from_epoch_ms() - ts);
-                }
-            }
-            ping::Responce::Timeout(_packet) => {
-                // The payload contains the EchoRequest packet + 64 bytes of payload
-                println!("Received timeout");
-            }
-            ping::Responce::Unreachable(_packet) => {
-                println!("Received unreachable");
-            }
-        }
-    }
-}
-
-/// Get the current time in milliseconds
-fn time_from_epoch_ms() -> u64 {
-    let start = SystemTime::now();
-    let since_the_epoch = start
-        .duration_since(UNIX_EPOCH)
-        .expect("Time went backwards");
-    let in_ms =
-        since_the_epoch.as_secs() * 1000 + since_the_epoch.subsec_nanos() as u64 / 1_000_000;
-    return in_ms;
+    run("10.0.2.15");
 }
